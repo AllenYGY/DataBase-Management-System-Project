20c5f717ff4bd984144bdd3dc276fcb619dfeec1
ALLENYGY <allenygy@outlook.com>
2023-11-28 00:58:13 +0800

    new

--- Project/code/00_index.php
+++ Project/code/00_index.php
@@ -1,94 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
- 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main Page</title>
-    <style>
-        * {
-            margin: 0;
-            padding: 0;
-            list-style: none;
-            text-decoration: none;
-            font-weight: 520;
-            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; 
-            /* 'Times New Roman', Times, serif; */
-            /* * {
-  font-family: 
-  box-sizing: border-box;
-} */
-        }
- 
-        #nav {
-            width: 1470px;
-            height: 72px;
-            background-color:#4284DB; 
-            /* rgb(247, 249, 250); */
-
-            border-radius: 20px;
-            margin: 20px auto;
-        }
- 
-        #nav>ul>li {
-            float: left;
-            width: 175.75px;
-            height: 72px;
-            text-align: center;
-        }
- 
-        #nav>ul>li>a {
-            text-align: center;
-            line-height: 72px;
-            font-size: 20px;
-            color: black;
-        }
- 
-        #nav>ul>li>a:hover {
-            color:  #29EAC4;
-            /* orange; */
-
-        }
- 
-        ul>span {
-            float: left;
-            line-height: 72px;
-            color: rgb(225, 224, 224);
-            font-weight: 800;
-            font-size: 24px;
-        }
- 
-        #nav>ul>li>.a1 {
-            color: black;
-        }
- 
-        #nav>ul>li>.a2 {
-            color:black; 
-            /* rgb(101, 198, 58); */
-        }
-    </style>
+    <link rel="stylesheet" href="index.css">
 </head>
- 
 <body>
     <div id="nav">
         <ul>
-            <li><a href="00_index.php" class="a1">Home</a></li><span>|</span>
-            <li></li><span></span>
-            <li></li><span>|</span>
-            <li><a href="#" class="a2">Search package</a></li><span>|</span>
-            <li><a href="#" class="a2">Package Status</a></li><span>|</span>
-
-            <li><a href="#" class="a2">Package History</a></li><span>|</span>
-            <!-- <li><a href="#"></a>dddd</li><span>|</span> -->
-            <!-- <li><a href="#"></a>gggg</li><span>|</span> -->
-            <!-- <li><a href="#"></a>hhhh</li> -->
-            <li><a href="01_login.php" class="a2">Login</a></li><span>|</span>
-            <li><a href="02_register.php" class="a2">Sign Up</a></li><span>|</span>
-            <!-- <li><a href="01_login.php"  class="a1"></a>Login</li><span>|</span> -->
-            <!-- <li><a href="02_register.php" class="a1"></a>Sign up</li><span>|</span> -->
+            <li><a href="00_index.php" class="a1">Home</a></li>
+            <li><a href="#" class="a2">Search</a></li>
+            <li><a href="#" class="a2">Status</a></li>
+            <li><a href="#" class="a2">History</a></li>
+            <li><a href="01_login.php" class="a2">Login</a></li>
+            <li><a href="02_register.php" class="a2">Sign Up</a></li>
         </ul>
     </div>
 </body>
- 
 </html>
