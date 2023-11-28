ca1059d56ede8069ab798e1d199c4d890dcfc9b3
ALLENYGY <allenygy@outlook.com>
2023-11-28 18:10:58 +0800

    new

--- Project/code/00_index.php
+++ Project/code/00_index.php
@@ -2,59 +2,254 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.4.11/dist/full.min.css" rel="stylesheet" type="text/css" />
-<script src="https://cdn.tailwindcss.com"></script>
-<!-- 引入样式 -->
-<link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">
-<!-- 引入组件库 -->
-<script src="https://unpkg.com/element-ui/lib/index.js"></script>
-    <title>Main Page</title>
-    <link rel="stylesheet" href="index.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <link rel="stylesheet" href="index.css">
 </head>
 
 <body>
-    <div class="navbar" id="navbar">
-        <ul>
-            <li><a href="00_index.php" class="a1">Home</a></li>
-            <li><a href="#" class="a2">Search</a></li>
-            <li><a href="#" class="a2">Pacakage Status</a></li>
-            <li><a href="#" class="a2">History</a></li>
-            <li><a href="01_login.php" class="a2">Login</a></li>
-            <li><a href="02_register.php" class="a2">Sign Up</a></li>
-        </ul>
-    </div>
-    <div class="sidebar" id="sidebar">
-        <ul>
-            <li>
-                <a href="00_index.php">
-                    <img src="https://cdn.jsdelivr.net/gh/ALLENYGY/ImageSpace@master/IMAGE/DBM/home.png" alt="Home" />
-                    <span class="text">Home</span>
-                </a>
-            </li>
-            <li><a href="#">Search</a></li>
-            <li><a href="#">Status</a></li>
-            <li><a href="#">History</a></li>
-        </ul>
-    </div>
+  <main>
+    <nav class="main-menu">
+      <h1>Courier Station</h1>
+      <img class="logo"
+        src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/4cfdcb5a-0137-4457-8be1-6e7bd1f29ebb"
+        alt="" />
+      <ul>
+        <li class="nav-item active">
+          <b></b>
+          <b></b>
+          <a href="#">
+            <i class="fa fa-house nav-icon"></i>
+            <span class="nav-text">Home</span>
+          </a>
+        </li>
 
-    <div class="content">
-        <div class="images-wrapper">
-            <div class="image-container">
-                <img src="/Project/image/pick.png" alt="Pick" />
-                <button id="normalBtn">Pick</button>
+        <li class="nav-item">
+          <b></b>
+          <b></b>
+          <a href="#">
+            <i class="fa fa-user nav-icon"></i>
+            <span class="nav-text">Profile</span>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <b></b>
+          <b></b>
+          <a href="#">
+            <i class="fa fa-calendar-check nav-icon"></i>
+            <span class="nav-text">Search</span>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <b></b>
+          <b></b>
+          <a href="#">
+            <i class="fa fa-person-running nav-icon"></i>
+            <span class="nav-text">History</span>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <b></b>
+          <b></b>
+          <a href="#">
+            <i class="fa fa-sliders nav-icon"></i>
+            <span class="nav-text">Settings</span>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <b></b>
+          <b></b>
+          <a href="01_login.php">
+            <i class="fa fa-sliders nav-icon"></i>
+            <span class="nav-text">Login</span>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <b></b>
+          <b></b>
+          <a href="02_register.php">
+            <i class="fa fa-sliders nav-icon"></i>
+            <span class="nav-text">Sign up</span>
+          </a>
+        </li>
+
+      </ul>
+    </nav>
+
+    <section class="content">
+      <div class="left-content">
+        <div class="activities">
+          <h1>Activities</h1>
+          <div class="activity-container">
+
+            <div class="image-container img-one">
+              <img src="/Project/image/pick.jpg" alt="pick" />
+              <div class="overlay">
+                <h3>Pick package</h3>
+              </div>
             </div>
-            <div class="image-container">
-                <img src="/Project/image/send.png" alt="Send" />
-                <button id="normalBtn">Send</button>
+
+            <div class="image-container img-two">
+              <img src="/Project/image/send.jpg" alt="send" />
+              <div class="overlay">
+                <h3>Send package</h3>
+              </div>
             </div>
+          </div>
         </div>
-        <button id="toggleBtn">Toggle</button>
 
-    </div>
-    <script src="script.js"></script>
+        <div class="left-bottom">
+          <div class="weekly-schedule">
+            <h1>Package status</h1>
+            <div class="calendar">
+              <div class="day-and-activity activity-one">
+                <div class="day">
+                  <h1>13</h1>
+                  <p>mon</p>
+                </div>
+                <div class="activity">
+                  <h2>Package</h2>
+                  <div class="participants">
+                  </div>
+                </div>
+                <button class="btn">Pick</button>
+              </div>
+
+              <div class="day-and-activity activity-two">
+                <div class="day">
+                  <h1>15</h1>
+                  <p>wed</p>
+                </div>
+                <div class="activity">
+                  <h2>Package</h2>
+                  <div class="participants">
+
+                  </div>
+                </div>
+                <button class="btn">Pick</button>
+              </div>
+
+              <div class="day-and-activity activity-three">
+                <div class="day">
+                  <h1>17</h1>
+                  <p>fri</p>
+                </div>
+                <div class="activity">
+                  <h2>Package</h2>
+                  <div class="participants">
+
+                  </div>
+                </div>
+                <button class="btn">Pick</button>
+              </div>
+
+              <div class="day-and-activity activity-four">
+                <div class="day">
+                  <h1>18</h1>
+                  <p>sat</p>
+                </div>
+                <div class="activity">
+                  <h2>Package</h2>
+                  <div class="participants">
+
+                  </div>
+                </div>
+                <button class="btn">Pick</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="personal-bests">
+            <h1>Express station status</h1>
+            <div class="personal-bests-container">
+              <div class="best-item box-one">
+                <p>Opening Hours:<br> 7 am. - 10 pm.</p>
+                <img
+                  src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/242bbd8c-aaf8-4aee-a3e4-e0df62d1ab27"
+                  alt="" />
+              </div>
+              <div class="best-item box-two">
+                <p>Crowded state:<br> Free </p>
+                <img
+                  src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/a3b3cb3a-5127-498b-91cc-a1d39499164a"
+                  alt="" />
+              </div>
+              <!-- <div class="best-item box-three">
+                <p>Longest Roller-Skating: 2 hours</p>
+                <img
+                  src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/e0ee8ffb-faa8-462a-b44d-0a18c1d9604c"
+                  alt="" />
+              </div> -->
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="right-content">
+        <div class="user-info">
+          <div class="icon-container">
+            <i class="fa fa-bell nav-icon"></i>
+            <i class="fa fa-message nav-icon"></i>
+          </div>
+          <h4>AllenYGY</h4>
+          <img src="https://cdn.jsdelivr.net/gh/ALLENYGY/ImageSpace@master/IMAGE/test.jpg" alt="user" />
+        </div>
+
+        <!-- <div class="active-calories">
+          <h1 style="align-self: flex-start">Active Calories</h1>
+          <div class="active-calories-container">
+            <div class="box" style="--i: 85%">
+              <div class="circle">
+                <h2>85<small>%</small></h2>
+              </div>
+            </div>
+            <div class="calories-content">
+              <p><span>Today:</span> 400</p>
+              <p><span>This Week:</span> 3500</p>
+              <p><span>This Month:</span> 14000</p>
+            </div>
+          </div>
+        </div> -->
+
+
+
+        <div class="friends-activity">
+          <h1>Friends' Package</h1>
+          <div class="card-container">
+            <div class="card">
+              <div class="card-user-info">
+                <img src="https://cdn.jsdelivr.net/gh/ALLENYGY/ImageSpace@master/IMAGE/DJY.png" alt="SJY"/>
+                <h2>SJY</h2>
+              </div>
+              <img class="card-img"
+                src="/Project/image/pick.jpg"
+                alt="pickup" />
+                <p>I just recive a new package today. Please help me pick up the package.</p>
+            </div>
+
+            <div class="card card-two">
+              <div class="card-user-info">
+                <img src="https://cdn.jsdelivr.net/gh/ALLENYGY/ImageSpace@master/IMAGE/DSX.png" alt="DSX" />
+                <h2>DSX</h2>
+
+              </div>
+              <img class="card-img"
+              src="/Project/image/pick.jpg"
+              alt="pickup" />
+              <p>I just recive a new package today. Please help me pick up the package.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  <script src="test.js"></script>
 </body>
 
 </html>
