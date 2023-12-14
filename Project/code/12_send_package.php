ec5101270738f9c2b336428d99ccc44e9b6c06bc
ALLENYGY <allenygy@outlook.com>
2023-12-14 09:41:41 +0800

    new

--- Project/code/12_send_package.php
+++ Project/code/12_send_package.php
@@ -6,8 +6,8 @@ session_start();
 $user = $_SESSION["user"];
 $usertype = $_SESSION["usertype"];
 $userID = $_SESSION["userID"];
-$csID=$_SESSION["csID"];
-$csadr=$_SESSION['csadr'];
+$csID = $_SESSION["csID"];
+$csadr = $_SESSION['csadr'];
 
 $url = '01_login.php';
 if ($usertype != 'cadmin') header('Location:' . $url);
