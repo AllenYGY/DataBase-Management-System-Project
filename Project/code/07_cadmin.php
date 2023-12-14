50b24215a35aeba755578e7caadc2decbdb79697
ALLENYGY <allenygy@outlook.com>
2023-12-14 09:25:40 +0800

    new

--- Project/code/07_cadmin.php
+++ Project/code/07_cadmin.php
@@ -13,6 +13,7 @@ include "03_connectDB.php";
 session_start();
 $user = $_SESSION["user"];
 $usertype = $_SESSION["usertype"];
+
 $url = '01_login.php';
 if ($usertype != 'cadmin') header('Location:' . $url);
 
