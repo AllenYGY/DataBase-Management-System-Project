794d518f9c7f8f7721655a5bc417628e9f3a2097
ALLENYGY <allenygy@outlook.com>
2023-12-14 09:22:48 +0800

    new

--- Project/code/12_send_package.php
+++ Project/code/12_send_package.php
@@ -5,17 +5,19 @@ session_start();
 
 $user = $_SESSION["user"];
 $usertype = $_SESSION["usertype"];
-$userID=$_SESSION["userID"];
+$userID = $_SESSION["userID"];
+$csID=$_SESSION["csID"];
+$csadr=$_SESSION['csadr'];
 
 $url = '01_login.php';
 if ($usertype != 'cadmin') header('Location:' . $url);
 
-// // getuserID
 $sql_update = "UPDATE parcel
-SET status = 'in_transit', send_time = NOW()
-WHERE status = 'pending' AND delivery_manageruID='$userID' ";
-$result=mysqli_query($conn,$sql_update);
-if($result){
+               SET status = 'in_transit', send_time = NOW()
+               WHERE status = 'pending' AND delivery_manageruID='$userID' ";
+
+$result = mysqli_query($conn, $sql_update);
+if ($result) {
   $url = '07_cadmin.php';
   header('Location:' . $url);
 }
