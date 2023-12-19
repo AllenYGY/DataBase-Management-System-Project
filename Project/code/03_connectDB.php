c894b5c850ace3b88273baa2291ce366a8adc017
ALLENYGY <allenygy@outlook.com>
2023-12-19 17:01:57 +0800

    new

--- Project/code/03_connectDB.php
+++ Project/code/03_connectDB.php
@@ -2,7 +2,7 @@
 
 $servername = "localhost"; // if use uic server, change to db.bcrab.cn
 $username     = "root";       // change to your account
-$password     = "040301Yjy";      // change to your account
+$password     = "";      // change to your account
 $db            = "project";      // change to your database name
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db);
