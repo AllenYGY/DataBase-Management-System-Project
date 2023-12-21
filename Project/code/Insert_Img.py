a9392c5561d9e014038e1c62e02cf7c4984e4892
ALLENYGY <allenygy@outlook.com>
2023-12-21 20:36:33 +0800

    new

---
+++ Project/code/Insert_Img.py
@@ -0,0 +1,23 @@
+import pymysql
+
+# 数据库连接参数
+servername = "localhost"
+username = "root"
+password = ""
+db = "project"
+
+# 建立数据库连接
+connection = pymysql.connect(host=servername, user=username, passwd=password, db=db)
+
+file_path = '/Users/allenygy/Desktop/DBM-Project/Project/image/user.png'  # 图片文件的路径
+with open(file_path, 'rb') as file:
+    binary_data = file.read()  # 读取文件内容为二进制数据
+
+try:
+    with connection.cursor() as cursor:
+        # 更新所有 upicture 为 NULL 的行
+        sql = "UPDATE customer SET upicture = %s WHERE upicture IS NULL"
+        cursor.execute(sql, (binary_data,))
+    connection.commit()
+finally:
+    connection.close()
