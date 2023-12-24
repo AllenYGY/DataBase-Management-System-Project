a8a3b3b4a6b7f66fa59346a9e194482ae49447b1
ALLENYGY <allenygy@outlook.com>
2023-12-24 13:45:28 +0800

    new

--- Project/code/09_edit_profile.php
+++ Project/code/09_edit_profile.php
@@ -4,6 +4,7 @@ include "03_connectDB.php";
 session_start();
 $user = $_SESSION["user"];
 $utype = $_SESSION["usertype"];
+$uID = $_SESSION["uID"];
 
 echo $utype;
 
@@ -14,9 +15,9 @@ $oldpwd = isset($_POST["oldpwd"]) ? $_POST["oldpwd"] : "";
 $editpwd = isset($_POST["editpwd"]) ? $_POST["editpwd"] : "";
 
 
-if($utype=='customer') $sql = "SELECT * FROM customer WHERE uname='$user'";
-if($utype=='cadmin') $sql = "SELECT * FROM cadmin WHERE uname='$user'";
-if($utype=='admin') $sql = "SELECT * FROM admin WHERE uname='$user'";
+if ($utype == 'customer') $sql = "SELECT * FROM customer WHERE uname='$user'";
+if ($utype == 'cadmin') $sql = "SELECT * FROM cadmin WHERE uname='$user'";
+if ($utype == 'admin') $sql = "SELECT * FROM admin WHERE uname='$user'";
 
 // echo $sql;
 
@@ -32,44 +33,79 @@ if ($result) {
     if ($utype == 'admin')
       $updateQuery = "UPDATE admin SET ";
     $check = 0;
-    if (!empty($newPhone)) {
-      $updateQuery .= " uphone='$newPhone', ";
-      $check = 1;
-    }
-    if (!empty($newMail)) {
-      $updateQuery .= " umail='$newMail', ";
-      $check = 1;
-    }
     if (!empty($newGender)) {
       $updateQuery .= " ugender='$newGender', ";
       $check = 1;
     }
-    
     if (!empty($editpwd)) {
       $updateQuery .= " upassword='$editpwd', ";
       $check = 1;
     }
-    // 移除最后一个逗号和空格
     $updateQuery = rtrim($updateQuery, ', ');
-    
-
     if ($check == 1) {
-      $updateQuery .= " WHERE uname='$user'";
+      $updateQuery .= " WHERE uID='$uID'";
       echo $updateQuery;
       $updateResult = mysqli_query($conn, $updateQuery);
 
-      if ($updateResult) {
-        echo "User information updated successfully!";
-        if ($utype == "customer") {
-          $url = "06_customer.php";
-        }
-        if ($utype == "cadmin")
-          $url = "07_cadmin.php";
-        if ($utype == "admin") {
-          $url = "08_admin.php";
-        }
-      } else {
-        echo "Error updating user information: " . mysqli_error($conn);
+      // if ($updateResult) {
+      //   echo "User information updated successfully!";
+      //   if ($utype == "customer") {
+      //     $url = "06_customer.php";
+      //   }
+      //   if ($utype == "cadmin")
+      //     $url = "07_cadmin.php";
+      //   if ($utype == "admin") {
+      //     $url = "08_admin.php";
+      //   }
+      // } else {
+        // echo "Error updating user information: " . mysqli_error($conn);
+      // }
+    }
+
+    if ($utype == 'customer')
+      $updateQuery = "UPDATE customer_email SET ";
+    if ($utype == 'cadmin')
+      $updateQuery = "UPDATE cadmin_email SET ";
+    if ($utype == 'admin')
+      $updateQuery = "UPDATE admin_email SET ";
+    $check = 0;
+    if (!empty($newMail)) {
+      $updateQuery .= " umail='$newMail' ";
+      $check = 1;
+    }
+    if ($check == 1) {
+      $updateQuery .= " WHERE uID='$uID'";
+      echo $updateQuery;
+      $updateResult = mysqli_query($conn, $updateQuery);
+      
+    }
+
+    if ($utype == 'customer')
+      $updateQuery = "UPDATE customer_phone SET ";
+    if ($utype == 'cadmin')
+      $updateQuery = "UPDATE cadmin_phone SET ";
+    if ($utype == 'admin')
+      $updateQuery = "UPDATE admin_phone SET ";
+    $check = 0;
+    if (!empty($newPhone)) {
+      $updateQuery .= " uphone='$newPhone' ";
+      $check = 1;
+    }
+    if ($check == 1) {
+      $updateQuery .= " WHERE uID='$uID'";
+      echo $updateQuery;
+      $updateResult = mysqli_query($conn, $updateQuery);
+    }
+
+    if ($updateResult) {
+      echo "User information updated successfully!";
+      if ($utype == "customer") {
+        $url = "06_customer.php";
+      }
+      if ($utype == "cadmin")
+        $url = "07_cadmin.php";
+      if ($utype == "admin") {
+        $url = "08_admin.php";
       }
     }
   } else {
